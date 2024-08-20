bc162c8a9634a442de6b176c6bf7214096d1213d
Niels Demke <niels.demke@hsu-hh.de>
2024-08-20 11:09:05 +0200

    apps and layout

--- script/script.js
+++ script/script.js
@@ -1,13 +1,27 @@
-
-// Email für CONTACT Button
 function openEmail() {
   var emailAddress = 'contact@used.community';
-  var subject = 'Nachhaltigkeitsanforderungen';
+  var subject = 'Anfrage';
   var body = '';
 
   var mailtoLink = 'mailto:' + encodeURIComponent(emailAddress) +
                    '?subject=' + encodeURIComponent(subject) +
                    '&body=' + encodeURIComponent(body);
-
   window.location.href = mailtoLink;
-  }
+}
+
+function searchApps() {
+  const input = document.getElementById('searchInput').value.toLowerCase();
+  const apps = document.querySelectorAll('.tool');
+  apps.forEach(app => {
+      const tags = app.getAttribute('data-tags').toLowerCase();
+      app.style.display = tags.includes(input) ? 'block' : 'none';
+  });
+}
+
+function filterApps(tag) {
+  const apps = document.querySelectorAll('.tool');
+  apps.forEach(app => {
+      const tags = app.getAttribute('data-tags').toLowerCase();
+      app.style.display = tag === '' || tags.includes(tag.toLowerCase()) ? 'block' : 'none';
+  });
+}
